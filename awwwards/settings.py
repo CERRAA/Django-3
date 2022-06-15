f9a1f788c138c59502f3be1fccb001fb704c3380
CERRAA <cerraangie113@gmail.com>
2022-06-15 14:31:39 +0300

    Deployment attempt

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'awwards.apps.AwwardsConfig',
     'members.apps.MembersConfig',
-
     'bootstrap4',
     'cloudinary',
     'crispy_forms',
@@ -58,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'awwwards.urls'
